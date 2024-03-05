d47f89efa1e02bb4e2d67cea92bc58dd44f703c8
이수민 <suminjeff@gmail.com>
2024-03-05 14:28:41 +0900

    chore: App.js boilerplate modified

--- frontend/voda/src/App.js
+++ frontend/voda/src/App.js
@@ -1,13 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div>
-      App Page
-    </div>
-  );
+  return <div>App Page</div>;
 }
 
 export default App;
