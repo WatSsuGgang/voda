ce8ae3e89f60ca5ddcf44af3411b2aefa61f42a7
김병주 <qudwn2941@naver.com>
2024-03-06 12:33:44 +0900

    chore: Create components

---
+++ frontend/voda/src/components/Button.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Button = () => {
+  return (
+    <button>
+      <h1>Button</h1>
+    </button>
+  );
+};
+
+export default Button;
---
+++ frontend/voda/src/components/CalenderDiary.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CalenderDiary = () => {
+  return <div>Calender Diary</div>;
+};
+
+export default CalenderDiary;
