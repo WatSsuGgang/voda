25c8dd6c9d89401bcbe0c899d869d0126f22ea00
이수민 <suminjeff@gmail.com>
2024-03-08 08:34:45 +0900

    chore: Added devoptions to manifest

--- frontend/voda/vite.config.js
+++ frontend/voda/vite.config.js
@@ -8,6 +8,9 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
       includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
       manifest: {
         name: "VODA",
