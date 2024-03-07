810b32fc49bd4a17ab3f89fdd076b3ab89731f71
이수민 <suminjeff@gmail.com>
2024-03-08 00:29:13 +0900

    chore: Modified manifest name to VODA

--- frontend/voda/vite.config.js
+++ frontend/voda/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
       registerType: "autoUpdate",
       includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
       manifest: {
-        name: "Voice Diary, VODA",
+        name: "VODA",
         short_name: "VODA",
         theme_color: "#ffffff",
         icons: [
