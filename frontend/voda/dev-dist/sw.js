9778941fa2231264dbf2002e76bae4656045b50e
이수민 <suminjeff@gmail.com>
2024-03-10 19:08:46 +0900

    feat(login): Add login page with social buttons

--- frontend/voda/dev-dist/sw.js
+++ frontend/voda/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.cs1uiksmf88"
+    "revision": "0.h7hcju5l09o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- frontend/voda/src/pages/login/Login.jsx
+++ frontend/voda/src/pages/login/Login.jsx
@@ -1,6 +1,53 @@
 import React from "react";
+import styled from "styled-components";
+import vodaLogo from "/logo.svg";
+import vodaLogoLetter from "/logo_letter.svg";
+import btn_google from "/login_btn/btn_google.svg";
+import btn_naver from "/login_btn/btn_naver.svg";
+import btn_kakao from "/login_btn/btn_kakao.svg";
+
+
+const ImageContainer = styled.div({
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  // height: "100vh",
+  width: "100vw",
+})
+
+
+const ButtonContainer = styled.div({
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  gap: "1rem",
+  marginTop: "3rem"
+})
+
+
 const Login = () => {
-  return <div>Login</div>;
+
+  return (
+  <div>
+    <ImageContainer>
+      <img src={vodaLogo} alt="" style={{
+        marginTop: "2.5rem",
+        height: "85%",
+        width: "85%",
+      }}/>
+      <img src={vodaLogoLetter} alt="" style={{
+        marginTop: "1.5rem"
+      }}/>
+    </ImageContainer>
+    <ButtonContainer>
+      <img src={btn_google} alt="" />
+      <img src={btn_kakao} alt="" />
+      <img src={btn_naver} alt="" />
+    </ButtonContainer>
+  </div>
+  );
 };
 
 export default Login;
