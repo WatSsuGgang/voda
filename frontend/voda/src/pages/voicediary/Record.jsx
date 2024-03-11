9b211613090bdbc2939c9b7d606e8a5a9d14453f
김병주 <qudwn2941@naver.com>
2024-03-11 17:47:00 +0900

    Chore: Create voice diary record page

--- frontend/voda/src/pages/voicediary/Record.jsx
+++ frontend/voda/src/pages/voicediary/Record.jsx
@@ -1,6 +1,60 @@
-import React from "react"; // eslint-disable-line no-unused-vars
+import React, { useState } from "react"; // eslint-disable-line no-unused-vars
+import styled from "styled-components";
+import LogoutIcon from "@mui/icons-material/Logout";
+import Timer from "../../components/voicediary/Timer";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+const Title = styled.h1`
+  text-align: center;
+  font-size: 1.2rem;
+  color: #afab99;
+`;
+
 const Record = () => {
-  return <div>Record</div>;
+  const { isListening } = useSpeechRecognition();
+  const [voiceRecognized] = useState(true);
+  const Emoticon = voiceRecognized ? (
+    <img
+      src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/People%20with%20activities/Man%20Gesturing%20OK%20Light%20Skin%20Tone.png"
+      alt="Man Gesturing OK Light Skin Tone"
+      width="300"
+      height="300"
+    />
+  ) : (
+    <img
+      src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/People%20with%20activities/Man%20Gesturing%20No%20Light%20Skin%20Tone.png"
+      alt="Man Gesturing No Light Skin Tone"
+      width="300"
+      height="300"
+    />
+  );
+  const Mention = voiceRecognized ? (
+    <div>잘 듣고 있어요. 계속 말씀하세요</div>
+  ) : (
+    <div>
+      음성을 인식하지 못했어요. <br />
+      다시 말씀해주세요
+    </div>
+  );
+  return (
+    <div>
+      <div style={{ marginTop: "15%", display: "flex", justifyContent: "end" }}>
+        <LogoutIcon />
+      </div>
+      <Title>AI와 대화하며 일기를 작성해요</Title>
+      <div style={{ textAlign: "center" }}>{Emoticon}</div>
+      <Timer />
+      <div
+        style={{ textAlign: "center", marginTop: "15%", fontSize: "1.2rem" }}
+      >
+        {Mention}
+      </div>
+    </div>
+  );
 };
 
 export default Record;
+
+// NO -> <img src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/People%20with%20activities/Man%20Gesturing%20No%20Light%20Skin%20Tone.png" alt="Man Gesturing No Light Skin Tone" width="300" height="300" />
+// OK -> <img src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/People%20with%20activities/Man%20Gesturing%20OK%20Light%20Skin%20Tone.png" alt="Man Gesturing OK Light Skin Tone" width="300" height="300" />
