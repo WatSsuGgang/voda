16b9f54c861d76738a1dec9f16e03c94c897288e
이수민 <suminjeff@gmail.com>
2024-03-11 15:00:31 +0900

    feat(mypage): Add mypage background image

--- frontend/voda/src/pages/mypage/MyPage.jsx
+++ frontend/voda/src/pages/mypage/MyPage.jsx
@@ -1,6 +1,18 @@
 import React from "react";
+import myPageBackground from "../../assets/mypage/mypage_background.png";
+
 const MyPage = () => {
-  return <div>MyPage</div>;
+  return (
+    <div
+      style={{
+        backgroundImage: `url(${myPageBackground})`,
+        backgroundSize: "cover",
+        height: "100vh",
+        // width: "100vw",
+        margin: "0 auto",
+      }}
+    ></div>
+  );
 };
 
 export default MyPage;
