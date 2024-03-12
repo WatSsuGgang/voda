732a41bb7bcaa4a7b28f1fd943bc2dc67cd77a3f
zqwz <zqwz@naver.com>
2024-03-12 10:53:04 +0900

    fix: Emotion label 접근제어자 변경

--- backend/src/main/java/io/watssuggang/voda/common/enums/Emotion.java
+++ backend/src/main/java/io/watssuggang/voda/common/enums/Emotion.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 public enum Emotion {
     JOY("01"), ANGER("02"), SADNESS("03"), FEAR("04"), CURIOSITY("05");
 
-    public final String label;
+    private final String label;
 
     Emotion(String label) {
         this.label = label;
