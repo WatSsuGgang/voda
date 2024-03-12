b862877c39ea704916a13137a322576d0e77ba78
zqwz <zqwz@naver.com>
2024-03-12 11:20:00 +0900

    fix: 음식, 효과 엔티티 수정
    Item 엔티티 상속하도록 변경
    Type 값 지정

--- backend/src/main/java/io/watssuggang/voda/pet/domain/Food.java
+++ backend/src/main/java/io/watssuggang/voda/pet/domain/Food.java
@@ -1,5 +1,10 @@
 package io.watssuggang.voda.pet.domain;
 
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("F")
 public class Food extends Item {
 
 }
