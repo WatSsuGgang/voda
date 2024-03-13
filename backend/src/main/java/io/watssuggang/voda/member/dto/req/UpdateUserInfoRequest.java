3c5f3a386fe2326f9edaf6ce7d8b28ff9463e500
sikutisa <ilgooinkorea@gmail.com>
2024-03-13 09:58:31 +0900

    feat: add updateUserInfo entry point
    Related issues: S10P22A104-132

---
+++ backend/src/main/java/io/watssuggang/voda/member/dto/req/UpdateUserInfoRequest.java
@@ -0,0 +1,12 @@
+package io.watssuggang.voda.member.dto.req;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class UpdateUserInfoRequest {
+
+    private String newNickname;
+
+}
