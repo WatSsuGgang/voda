d8308fe2f33d504abb6fbacde9a998a9f4a3372b
이수민 <suminjeff@gmail.com>
2024-03-13 17:24:05 +0900

    feat(calendar): Fix some style

--- frontend/voda/dev-dist/sw.js
+++ frontend/voda/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.3l45s60k6v"
+    "revision": "0.c1e71plb4m"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
