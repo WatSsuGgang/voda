c76c7a577f247f90dafbd43a0a69fd87745a8cb7
이수민 <suminjeff@gmail.com>
2024-03-13 10:47:18 +0900

    feat(login): Modify nickname, signupbutton

--- frontend/voda/dev-dist/sw.js
+++ frontend/voda/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.d1k29315o4g"
+    "revision": "0.23fs6d3ccng"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
