ff808225a16c1186919df185d9477f2f06372921
zqwz <zqwz@naver.com>
2024-03-13 13:03:47 +0900

    feat: BaseEntity Getter 추가

--- backend/src/main/java/io/watssuggang/voda/common/domain/BaseEntity.java
+++ backend/src/main/java/io/watssuggang/voda/common/domain/BaseEntity.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
@@ -12,6 +13,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
+@Getter
 public abstract class BaseEntity {
 
     @CreatedDate
