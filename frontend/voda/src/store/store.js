f7a7b426d9a5727de3aed37dd8fc397db18632ef
김병주 <qudwn2941@naver.com>
2024-03-13 15:47:11 +0900

    Feat: create diary list page
    Related to: S10P22A104-101

--- frontend/voda/src/store/store.js
+++ frontend/voda/src/store/store.js
@@ -2,6 +2,15 @@ import { create } from "zustand";
 import { devtools } from "zustand/middleware"; // 디버깅 용도
 
 const store = (set, get) => ({
+  emotions: {
+    anger:
+      "https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Smilies/Enraged%20Face.png",
+    joy: "https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Smilies/Grinning%20Face%20with%20Smiling%20Eyes.png",
+    sad: "https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Smilies/Loudly%20Crying%20Face.png",
+    fear: "https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Smilies/Fearful%20Face.png",
+    curiosity:
+      "https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Smilies/Nerd%20Face.png",
+  },
   editAllow: true,
   isLoggedIn: true,
   nickname: "갓소민",
