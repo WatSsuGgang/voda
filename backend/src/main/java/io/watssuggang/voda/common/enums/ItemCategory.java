19cc6a4dff845669ff2a1007b0becf4c2ee76be2
zqwz <zqwz@naver.com>
2024-03-14 13:50:15 +0900

    feat: 아이템 코드 컨버터 등록

--- backend/src/main/java/io/watssuggang/voda/common/enums/ItemCategory.java
+++ backend/src/main/java/io/watssuggang/voda/common/enums/ItemCategory.java
@@ -1,6 +1,23 @@
 package io.watssuggang.voda.common.enums;
 
-public enum ItemCategory {
+import io.watssuggang.voda.common.converter.AbstractLabelConverter;
+import lombok.Getter;
 
-    FOOD, EFFECT;
+@Getter
+public enum ItemCategory implements LabelEnum {
+
+    FOOD("01"), EFFECT("02");
+    private final String label;
+
+    ItemCategory(String label) {
+        this.label = label;
+    }
+
+    @jakarta.persistence.Converter(autoApply = true)
+    static class ConverterAbstract extends AbstractLabelConverter<ItemCategory> {
+
+        public ConverterAbstract() {
+            super(ItemCategory.class);
+        }
+    }
 }
