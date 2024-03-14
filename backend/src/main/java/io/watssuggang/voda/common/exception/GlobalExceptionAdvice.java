557f0115fb78a462a39875636e2133ce43c2ecfb
zqwz <zqwz@naver.com>
2024-03-14 11:13:44 +0900

    feat: MethodArgumentNotValidException 핸들러, response 추가

--- backend/src/main/java/io/watssuggang/voda/common/exception/GlobalExceptionAdvice.java
+++ backend/src/main/java/io/watssuggang/voda/common/exception/GlobalExceptionAdvice.java
@@ -1,10 +1,27 @@
 package io.watssuggang.voda.common.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionAdvice {
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationErrorResponse> handleValidationExceptions(
+            MethodArgumentNotValidException e
+    ) {
+        ValidationErrorResponse errorResponse = new ValidationErrorResponse();
+        e.getBindingResult().getFieldErrors().forEach(
+                fieldError -> errorResponse.addFieldError(
+                        fieldError.getField(),
+                        fieldError.getDefaultMessage()
+                )
+        );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
 }
