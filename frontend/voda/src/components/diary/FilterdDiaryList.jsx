9494959c26f0a3b5ccd5abf76405e7746cc6827f
김병주 <qudwn2941@naver.com>
2024-03-14 14:41:14 +0900

    Fix: Add function detail diary page(save image)
    Related to: S10P22A104-102

--- frontend/voda/src/components/diary/FilterdDiaryList.jsx
+++ frontend/voda/src/components/diary/FilterdDiaryList.jsx
@@ -30,6 +30,8 @@ const FilterdDiaryList = () => {
       title: "힘들어서 한 잔 했습니다",
       date: "2020-01-01",
       emotion: "sad",
+      content:
+        "하... 인생이 많이 쓰다. 오늘은 술 한 잔 했다. 술이 달게 느껴진다. 진호는 왜 이렇게 잘 마시는거야? 피파 잘 하고 싶다. 가지 덮밥 생각보다 맛있다. Skrrr",
       picture:
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRavVXxGrKkOn7bp0JcTSNO8yBlXnw4fihPPg&usqp=CAU",
     },
@@ -38,6 +40,8 @@ const FilterdDiaryList = () => {
       title: "힘들어서 한 잔 했습니다",
       date: "2020-01-01",
       emotion: "sad",
+      content:
+        "하... 인생이 많이 쓰다. 오늘은 술 한 잔 했다. 술이 달게 느껴진다. 진호는 왜 이렇게 잘 마시는거야? 피파 잘 하고 싶다. 가지 덮밥 생각보다 맛있다. Skrrr",
       picture:
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRavVXxGrKkOn7bp0JcTSNO8yBlXnw4fihPPg&usqp=CAU",
     },
@@ -46,6 +50,8 @@ const FilterdDiaryList = () => {
       title: "힘들어서 한 잔 했습니다",
       date: "2020-01-01",
       emotion: "sad",
+      content:
+        "하... 인생이 많이 쓰다. 오늘은 술 한 잔 했다. 술이 달게 느껴진다. 진호는 왜 이렇게 잘 마시는거야? 피파 잘 하고 싶다. 가지 덮밥 생각보다 맛있다. Skrrr",
       picture:
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRavVXxGrKkOn7bp0JcTSNO8yBlXnw4fihPPg&usqp=CAU",
     },
--- frontend/voda/src/pages/diarylist/DiaryDetail.jsx
+++ frontend/voda/src/pages/diarylist/DiaryDetail.jsx
@@ -1,24 +1,53 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useLocation } from "react-router";
 import useStore from "../../store/store";
 import styled from "styled-components";
 import trash from "../../assets/diarylist/trash.svg";
 import save from "../../assets/diarylist/save.svg";
+import playbutton from "../../assets/diarylist/playbutton.svg";
 const Title = styled.h3`
   color: #486b73;
 `;
 
 const Container = styled.div`
   background-color: #f1f1f1;
-  width: 20rem;
-  height: 25rem;
-  margin: 0 auto;
+  min-width: 90vw;
+  min-height: 30vh;
+  margin: 0 10px;
   border-radius: 15px;
+  max-height: 62vh;
+  overflow: auto;
 `;
 const DiaryDetail = () => {
   const store = useStore();
   const { state } = useLocation();
   const emotionImageUrl = store.emotions[state.emotion];
+  const deleteDiary = (id) => {
+    // api delete 요청 보내는 로직 구현 예정
+  };
+
+  const playVoice = () => {
+    // 음성 파일을 재생시키는 로직 구현 예정
+  };
+  const handleDownloadClick = (url) => {
+    fetch(url, {
+      method: "GET",
+    })
+      .then((res) => {
+        return res.blob();
+      })
+      .then((blob) => {
+        const blobUrl = URL.createObjectURL(blob);
+        const aTag = document.createElement("a");
+        aTag.href = blobUrl;
+        aTag.download = `${state.date}.png`;
+        aTag.click();
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+  };
+
   return (
     <div>
       <div
@@ -49,21 +78,37 @@ const DiaryDetail = () => {
         >
           {state.title}
         </div>
+        {/* 상단 바 -> 사진 저장, 날짜, 게시글 삭제 버튼 */}
         <div
           style={{
             display: "flex",
             alignItems: "center",
             justifyContent: "space-between",
+            margin: "0 0.8rem",
           }}
         >
-          <img src={save} alt="" />
+          <img src={save} onClick={() => handleDownloadClick(state.picture)} />
           <div style={{ display: "flex" }}>
             {state.date}
-            <img src={trash} alt="" />
+            <img src={trash} style={{ marginLeft: "0.5rem" }} />
           </div>
         </div>
-        <div>내용입니다.</div>
+        {/* 일기 이미지 */}
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <img
+            src={state.picture}
+            style={{ width: "80%", height: "50%", borderRadius: "10%" }}
+          />
+        </div>
+        {/* 일기 내용 */}
+        <div style={{ padding: "2rem" }}>{state.content}</div>
       </Container>
+      {/* 녹음 파일 재생 버튼 */}
+      <div
+        style={{ display: "flex", justifyContent: "center", marginTop: "2rem" }}
+      >
+        <img src={playbutton} />
+      </div>
     </div>
   );
 };
