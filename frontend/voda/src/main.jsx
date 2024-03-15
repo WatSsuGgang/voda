693e2e4ca54ece054b93cce2eebf7f4e0e4c259c
이수민 <suminjeff@gmail.com>
2024-03-15 10:27:44 +0900

    feat(auth): Add api services, auth api

--- frontend/voda/src/main.jsx
+++ frontend/voda/src/main.jsx
@@ -5,7 +5,6 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import axios from "axios";
 
-axios.defaults.baseURL = import.meta.env.VITE_API_URL;
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
