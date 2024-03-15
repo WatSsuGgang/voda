f2f82d0c4a0d3dd7607c0789990cfd174006ce6a
이수민 <suminjeff@gmail.com>
2024-03-15 10:27:44 +0900

    feat(auth): Add api services, auth api

--- frontend/voda/src/services/auth.js
+++ frontend/voda/src/services/auth.js
@@ -1,33 +1,27 @@
 import axios from "axios";
-import { postRequest } from "./api";
+import { postData } from "./api";
 
 // 회원가입 요청
-export const signUp = async (data, config) => {
-  const response = postRequest("/auth/signup", data, config);
-
-  // try {
-  //   const response = await axios.post("/auth/signup", data, config);
-  //   if (response.status === 200) {
-  //     window.alert("회원가입 성공");
-  //   } else {
-  //     console.log(response);
-  //     return alert("RESPONSE NOT 200");
-  //   }
-  // } catch (error) {
-  //   console.error(error);
-  //   return alert("ERROR");
-  // }
+export const signUp = async (data) => {
+  const response = postData("/auth/signup", data);
+  if (response) {
+    // response가 true를 리턴하면 다시 로그인 페이지로
+  } else {
+    // response가 false라면 회원가입 실패, 다시 회원가입 페이지로
+  }
 };
 
 // 토큰을 갱신하려면 만료된 토큰을 headers에 보내줘야 함
 // tokenRefresh의 정상적인 리턴값은 새 accessToken
-export const tokenRefresh = async (config, func) => {
+export const tokenRefresh = async () => {
   try {
-    const response = await axios.post("/token/refresh", {}, config);
-    if (response.status === 200) {
+    const response = postData("/token/refresh");
+    if (response) {
       const newAccessToken = response.data.accessToken;
-      return newAccessToken;
+      localStorage.setItem("accessToken", newAccessToken);
+      return;
     } else {
+      // refresh 토큰도 만료됐으면 다시 로그인
       console.log(response);
       return alert("RESPONSE NOT 200");
     }
