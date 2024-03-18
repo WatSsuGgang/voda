6816528ac337a7ccec394e68dd448a991ed780aa
이수민 <suminjeff@gmail.com>
2024-03-18 11:04:55 +0900

    feat(api): Add api reques boilerplate

---
+++ frontend/voda/src/services/item.js
@@ -0,0 +1,38 @@
+import { request } from "./api";
+import { HTTPMethods } from "./api";
+import { HTTPStatusCodes } from "./api";
+
+// Admin
+// 아이템 생성
+export const createItem = (data) => {
+  const url = `/item`;
+  const response = request(HTTPMethods.POST, url, data);
+};
+
+// Admin
+// 아이템 수정
+export const editItem = (data) => {
+  const url = `/item`;
+  const response = request(HTTPMethods.PATCH, url, data);
+};
+
+// Admin
+// 아이템 삭제
+export const deleteItem = () => {
+  const url = `/item`;
+  const response = request(HTTPMethods.DELETE, url);
+};
+
+// User
+// 아이템 구매
+export const buyItem = (data) => {
+  const url = `/item/buy`;
+  const response = request(HTTPMethods.POST, url, data);
+};
+
+// All
+// 상점 아이템 조회
+export const getItem = (category) => {
+  const url = `/item?category=${category}`;
+  const response = request(HTTPMethods.GET, url);
+};
