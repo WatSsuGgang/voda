b704b96bae6bc74b1ac30d75bef8aae6a710dd97
이수민 <suminjeff@gmail.com>
2024-03-19 17:55:17 +0900

    feat(pet): Fix petshop bugs

--- frontend/voda/src/components/shop/Header.jsx
+++ frontend/voda/src/components/shop/Header.jsx
@@ -1,18 +1,36 @@
 import React from "react";
+import usePetStore from "../../store/petStore";
 import styled from "styled-components";
 
 const HeaderComponent = styled.div({
   display: "flex",
-  justifyContent: "flex-start",
+  justifyContent: "space-between",
   alignItems: "center",
   width: "85%",
   borderBottom: "1px solid #ccc",
 });
 
 export default function Header() {
+  const { currentCoin } = usePetStore();
   return (
     <HeaderComponent>
       <h3>상점</h3>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "0.5rem",
+        }}
+      >
+        <img
+          src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Objects/Coin.png"
+          alt="Coin"
+          width="40rem"
+          height="40rem"
+        />
+        <h3>{currentCoin}</h3>
+      </div>
     </HeaderComponent>
   );
 }
