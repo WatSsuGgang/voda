7b98e34240d0af258bf9ec8188015fc0ccf7f24a
이수민 <suminjeff@gmail.com>
2024-03-20 17:43:43 +0900

    feat(pet): Pet data connect with backend

--- frontend/voda/src/pages/pet/PetPage.jsx
+++ frontend/voda/src/pages/pet/PetPage.jsx
@@ -1,11 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import TopBar from "../../components/pet/TopBar";
 import TopComponent from "../../components/pet/TopComponent";
 import MiddleComponent from "../../components/pet/MiddleComponent";
 import Pet from "../../components/pet/Pet";
 import PetTalk from "../../components/pet/PetTalk";
+import { getPet } from "../../services/pet";
 
+// data 형식
+// data = {owned: [], pet: {emotion: "JOY", exp: 0, isEvolution: false, isFeed: true}}
 const PetPage = () => {
+  const [owned, setOwned] = useState([]);
+  const [emotion, setEmotion] = useState(null);
+  const [exp, setExp] = useState(null);
+  const [isEvolution, setIsEvolution] = useState(null);
+  const [isFeed, setIsFeed] = useState(null);
+  const [level, setLevel] = useState(null);
+  const [name, setName] = useState(null);
+  const [petAppearance, setPetAppearance] = useState(null);
+  const [petId, setPetId] = useState(null);
+  const [stage, setStage] = useState(null);
+
+  useEffect(() => {
+    // 임시 memberId
+    const fetchData = async () => {
+      try {
+        const response = await getPet(1);
+        setOwned(response.owned);
+        setEmotion(response.pet.emotion);
+        setExp(response.pet.exp);
+        setIsEvolution(response.pet.isEvolution);
+        setIsFeed(response.pet.isFeed);
+        setLevel(response.pet.level);
+        setName(response.pet.name);
+        setPetAppearance(response.pet.petAppearance);
+        setPetId(response.pet.petId);
+        setStage(response.pet.stage);
+        return response;
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
   return (
     <div
       style={{
