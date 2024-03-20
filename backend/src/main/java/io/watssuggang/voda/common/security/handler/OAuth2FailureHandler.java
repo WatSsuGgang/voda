0d5517ecbbd86375f8f87e97709d37d921d138eb
김진호 <lantern50@naver.com>
2024-03-20 10:02:22 +0900

    Fix: 로그인 redirect URL 수정

--- backend/src/main/java/io/watssuggang/voda/common/security/handler/OAuth2FailureHandler.java
+++ backend/src/main/java/io/watssuggang/voda/common/security/handler/OAuth2FailureHandler.java
@@ -13,6 +13,6 @@ public class OAuth2FailureHandler implements AuthenticationFailureHandler {
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
         AuthenticationException exception) throws IOException {
-        response.sendRedirect("https://j10a104.p.ssafy.io//login");
+        response.sendRedirect("https://j10a104.p.ssafy.io/login");
     }
 }
