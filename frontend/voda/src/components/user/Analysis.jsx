61c4c8c9ef3c034501f2d7b7c5aeccda1edfd42f
김병주 <qudwn2941@naver.com>
2024-03-22 16:57:13 +0900

    feat: user statistic analysis

---
+++ frontend/voda/src/components/user/Analysis.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+import AnalysisDaily from "./AnalysisDaily";
+import AnalysisEmotion from "./AnalysisEmotion";
+import AnalysisPet from "./AnalysisPet";
+import AnalysisCount from "./AnalysisCount";
+const Analysis = ({ report }) => {
+  return (
+    <div>
+      {/* <AnalysisCount report={report} /> */}
+      <AnalysisEmotion report={report} />
+      {/* <AnalysisDaily report={report} /> */}
+      {/* <AnalysisPet report={report} /> */}
+    </div>
+  );
+};
+
+export default Analysis;
