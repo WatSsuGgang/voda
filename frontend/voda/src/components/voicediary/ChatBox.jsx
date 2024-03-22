db432c73edbc6a8edf38c6290c49b3f1fd1c9a07
김병주 <qudwn2941@naver.com>
2024-03-22 09:32:32 +0900

    fix: voicediary components

--- frontend/voda/src/components/voicediary/ChatBox.jsx
+++ frontend/voda/src/components/voicediary/ChatBox.jsx
@@ -1,21 +1,86 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import { getTalkList } from "../../services/voicediary"; // 대화 내역 불러오는 API 함수
 const Container = styled.div`
   margin: 3vh 1vh;
   max-height: 55vh;
   overflow: auto;
 `;
 
-const ChatBox = (props) => {
+const ChatBox = ({ onChildDataChange }) => {
   // 음성 일기 더미 데이터. 화면 이동할 때, props로 넘겨주는 방식으로 구현 예정.
-  // const questions = []
-  // const answers =[]
+  // const [chatMessages, setChatMessages] = useState([]);
+
   // useEffect(() => {
-  //   props.map(data => {
-  //    questions.push(data[q])
-  //    answers.push(data[a])
-  //   })
-  // }, [])
+  //   const fetchData = async () => {
+  //     const res = await getTalkList(diaryId);
+  //     setChatMessages(res[talk_list]);
+  //   };
+  //   fetchData();
+  // }, []);
+
+  // // 텍스트 수정이 일어날 때마다
+  // useEffect(() => {
+  //   onChildDataChange(chatMessages)
+  // }, [chatMessages])
+
+  // // 변화 추적
+  // const handleChange = (index, value) => {
+  //   const newContents = [...chatMessages];
+  //   newContents[index] = value;
+  //   setChildData(newContents);
+  // };
+
+  // const renderInputs = () => {
+  //   return chatMessages.map((content, index) => {
+  //     if (index % 2 === 1) {
+  //       return (
+  //         <div
+  //           style={{ display: "flex", justifyContent: "end", margin: "3vh 0" }}
+  //         >
+  //           <input
+  //             style={{
+  //               border: "0",
+  //               backgroundColor: "#D8E5FF",
+  //               borderRadius: "10px",
+  //               padding: "20px",
+  //             }}
+  //             key={index}
+  //             placeholder={content}
+  //             value={content}
+  //             onChange={(e) => handleChange(index, e.target.value)}
+  //           />
+  //         </div>
+  //       );
+  //     } else {
+  //       return (
+  //         <div
+  //           style={{
+  //             display: "flex",
+  //             justifyContent: "start",
+  //             margin: "3vh 0",
+  //           }}
+  //         >
+  //           <input
+  //             style={{
+  //               border: "0",
+  //               backgroundColor: "#F2F2F2",
+  //               borderRadius: "10px",
+  //               padding: "20px",
+  //             }}
+  //             key={index}
+  //             placeholder={content}
+  //             value={content}
+  //             disabled
+  //           />
+  //         </div>
+  //       );
+  //     }
+  //   });
+  // };
+
+  // return <Container>{renderInputs()}</Container>;
+
   const questions = [
     "오늘 하루는 어땠어?",
     "왜 기분이 안 좋았어?",
@@ -34,7 +99,6 @@ const ChatBox = (props) => {
 
   const [chatContents, setChatContents] = useState(combinedContents);
 
-  //   console.log(chatContents);
   const handleChange = (index, value) => {
     const newContents = [...chatContents];
     newContents[index] = value;
