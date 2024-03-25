697dd3e06795b4ed544c303943686b111c2b72d0
이수민 <suminjeff@gmail.com>
2024-03-25 16:36:15 +0900

    feat(calendar): Calendar WIP

--- frontend/voda/src/pages/calendar/CalendarPage.jsx
+++ frontend/voda/src/pages/calendar/CalendarPage.jsx
@@ -1,7 +1,16 @@
-import React from "react"; // eslint-disable-line no-unused-vars
+import React, { useEffect } from "react"; // eslint-disable-line no-unused-vars
 import Calendar from "../../components/calendar/Calendar";
+import { getMonth } from "../../services/calendar";
 
 const CalendarPage = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await getMonth();
+      console.log(response);
+    };
+    fetchData();
+  }, []);
+
   return (
     <>
       <h2>감정 캘린더</h2>
--- frontend/voda/src/services/calendar.js
+++ frontend/voda/src/services/calendar.js
@@ -5,9 +5,17 @@ import { HTTPStatusCodes } from "./api";
 // User
 //
 // month와 year 파라미터 안 넘어오면 현재 시간을 기준으로
+export const getMonthNow = () => {
+  const url = `/calendar`;
+  const response = request(HTTPMethods.GET, url);
+  return response;
+};
+
+// month와 year 파라미터 안 넘어오면 현재 시간을 기준으로
 export const getMonth = (month, year) => {
   const url = `/calendar?month=${month}&year=${year}`;
   const response = request(HTTPMethods.GET, url);
+  return response;
 };
 
 // User
