ba89bb92dcb37b975787ecac4c8375ab20fa5f4b
sikutisa <ilgooinkorea@gmail.com>
2024-03-25 15:06:14 +0900

    feat: add getMonthlyDiaries business logic
    Related to: S10P22A104-135

--- backend/src/main/java/io/watssuggang/voda/diary/controller/CalendarController.java
+++ backend/src/main/java/io/watssuggang/voda/diary/controller/CalendarController.java
@@ -3,6 +3,7 @@ package io.watssuggang.voda.diary.controller;
 import io.watssuggang.voda.common.security.annotation.CurrentUser;
 import io.watssuggang.voda.common.security.dto.SecurityUserDto;
 import io.watssuggang.voda.diary.dto.req.MonthlyDiariesReqDto;
+import io.watssuggang.voda.diary.dto.res.MonthlyDiariesResDto;
 import io.watssuggang.voda.diary.service.CalendarService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -18,7 +19,7 @@ public class CalendarController {
     @GetMapping
     public ResponseEntity<?> getMonthlyDiaries(@CurrentUser SecurityUserDto currentUser,
         MonthlyDiariesReqDto params) {
-        MonthlyDiariesReqDto response = calendarService.getMonthlyDiaries(currentUser,
+        MonthlyDiariesResDto response = calendarService.getMonthlyDiaries(1,
             params.getMonth(), params.getYear());
 
         return ResponseEntity.ok(response);
