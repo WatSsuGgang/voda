793686439fdeeeb9500772295dd80d57696d6004
김진호 <lantern50@naver.com>
2024-03-26 14:28:37 +0900

    fix: 원복

--- frontend/voda/vite.config.js
+++ frontend/voda/vite.config.js
@@ -1,36 +1,49 @@
 import { defineConfig } from "vite";
-//import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    //react(),
     VitePWA({
-      //selfDestroying: true, //임시용 나중에 삭제해야됨
-      strategies: 'injectManifest',
-      srcDir: '/',
-      swSrc: 'voda-sw.js',
-      swDest: 'dist/voda-sw.js',
-      filename: 'voda-sw.js',
-      manifest: '/pwa-manifest.json',
-      manifestFilename: 'pwa-manifest.json',
-      injectRegister: 'none',   // 스크립트 자동 삽입 비활성화
-      registerType: "prompt",
-      //registerType: "autoUpdate",
-      includeAssets: ['**/*.(js|css|svg|ico|png'],
+      selfDestroying: true,
+      registerType: "autoUpdate",
       devOptions: {
         enabled: true,
       },
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+      manifest: {
+        name: "VODA",
+        short_name: "VODA",
+        display: "standalone",
+        orientation: "portrait",
+        theme_color: "#ffffff",
+        background_color: "#FFFAE1",
+        start_url: "/",
+        icons: [
+          {
+            src: "pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
     }),
   ],
-
-  build: {
-    rollupOptions: {
-      input: {
-        main: "./index.html",
-        sw: "./voda-sw.js",
-      },
-    },
-  },
 });
