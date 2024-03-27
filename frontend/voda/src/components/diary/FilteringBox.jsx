5a1a78651661abcc726bacf1f9732c87865a9d66
김병주 <qudwn2941@naver.com>
2024-03-27 11:25:56 +0900

    Fix: filteringbox update

--- frontend/voda/src/components/diary/FilteringBox.jsx
+++ frontend/voda/src/components/diary/FilteringBox.jsx
@@ -1,4 +1,4 @@
-import { React, useState } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
