f7a64e2fe3943d84c0d75ec0266091b1a8cf96c0
이수민 <suminjeff@gmail.com>
2024-03-27 17:37:11 +0900

    feat(calendar): Fixing calendar logic

--- frontend/voda/src/components/calendar/Calendar.jsx
+++ frontend/voda/src/components/calendar/Calendar.jsx
@@ -1,14 +1,31 @@
 // Calendar.js
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./styles.css"; // styles.css 파일 import
 import CalendarDetail from "./CalendarDetail";
+import { getMonth } from "../../services/calendar";
 
 const Calendar = () => {
-  const [selectedDate, setSelectedDate] = useState(null);
+  const [selectedDate, setSelectedDate] = useState(new Date().toDateString()); // 현재 날짜로 초기화
   const [showDetail, setShowDetail] = useState(false);
   const [currentMonth, setCurrentMonth] = useState(new Date().getMonth());
   const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
+  const [diaryDates, setDiaryDates] = useState([]); // 백엔드 API에서 받아온 일기가 있는 날짜들을 저장
+
+  useEffect(() => {
+    // 백엔드 API에서 해당 월의 일기가 있는 날짜들을 받아옴
+    const fetchDiaryDates = async () => {
+      try {
+        const response = await getMonth(currentMonth, currentYear);
+        setDiaryDates(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.error("Error fetching diary dates:", error);
+      }
+    };
+
+    fetchDiaryDates();
+  }, [currentMonth, currentYear]);
 
   const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate(); // 현재 월의 일 수
   const firstDayOfMonth = new Date(currentYear, currentMonth, 1).getDay(); // 현재 월의 첫 날의 요일 (0: 일요일, 1: 월요일, ..., 6: 토요일)
@@ -46,11 +63,16 @@ const Calendar = () => {
     // 날짜를 추가합니다.
     for (let i = 1; i <= daysInMonth; i++) {
       const date = new Date(currentYear, currentMonth, i);
+      const isCurrentMonth = date.getMonth() === currentMonth; // 현재 달인지 확인
+      const isSelected = date.toDateString() === selectedDate; // 선택된 날짜인지 확인
+
       calendarDays.push(
         <div
           key={i}
-          className="calendar-day"
-          onClick={() => handleDateClick(date)}
+          className={`calendar-day ${
+            isCurrentMonth ? "current-month" : "non-current-month"
+          } ${isSelected ? "selected" : ""}`} // 현재 달이 아닌 경우에는 'non-current-month' 클래스 추가
+          onClick={() => isCurrentMonth && handleDateClick(date)} // 현재 달이 아닌 날짜는 클릭 불가능하도록 설정
         >
           {i}
         </div>
@@ -81,6 +103,14 @@ const Calendar = () => {
           </div>
         ))}
       </div>
+      {showDetail && (
+        <div
+          className="modal-overlay"
+          onClick={() => {
+            setShowDetail(false);
+          }}
+        ></div>
+      )}
       {renderCalendar()}
       {showDetail && (
         <CalendarDetail
