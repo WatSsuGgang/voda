23df333f5fcb7094977ab2311a821348476aa64a
zqwz <zqwz@naver.com>
2024-03-27 15:56:38 +0900

    fix: 서비스 워커 파일 수정

--- frontend/voda/voda-sw.js
+++ frontend/voda/voda-sw.js
@@ -38,4 +38,4 @@ self.addEventListener("fetch", (event) => {
 // 추가: 매니페스트를 프리캐시하고 라우팅
 self.__WB_MANIFEST = [].concat(self.__WB_MANIFEST || []);
 
-precacheAndRoute(self.__WB_MANIFEST);
+// precacheAndRoute(self.__WB_MANIFEST);
