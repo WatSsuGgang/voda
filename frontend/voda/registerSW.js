06f571df5373ee2cf3400eb546c5baf8aa515eee
김진호 <lantern50@naver.com>
2024-03-28 09:53:49 +0900

    fix: pwa 설정 수정

--- frontend/voda/registerSW.js
+++ frontend/voda/registerSW.js
@@ -1,5 +1,5 @@
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/voda-sw.js', { scope: '/' })
-    })
-  }
+// if ('serviceWorker' in navigator) {
+//     window.addEventListener('load', () => {
+//       navigator.serviceWorker.register('/voda-sw.js', { scope: '/' })
+//     })
+//   }
