55059d9eeedbf89c41c27c8915d083f97dca40bb
김병주 <qudwn2941@naver.com>
2024-03-29 10:49:57 +0900

    Feat: install button add

--- frontend/voda/src/pages/login/Introduction.jsx
+++ frontend/voda/src/pages/login/Introduction.jsx
@@ -4,6 +4,30 @@ import IntroductionHeader from "../../components/login/IntroductionHeader";
 import LoginButton from "../../components/login/LoginButton";
 
 const Introduction = () => {
+  // if ("serviceWorker" in navigator) {
+  //   window.addEventListener("load", async () => {
+  //     try {
+  //       const registration = await navigator.serviceWorker.register("/sw.js");
+  //       console.log(
+  //         "ServiceWorker registration successful with scope: ",
+  //         registration.scope
+  //       );
+  //     } catch (err) {
+  //       console.error("ServiceWorker registration failed: ", err);
+  //     }
+  //   });
+  // }
+
+  // window.addEventListener("load", () => {
+  //   if (
+  //     navigator.serviceWorker &&
+  //     window.matchMedia("(display-mode: standalone)").matches
+  //   ) {
+  //     navigator.serviceWorker.getRegistration().then((reg) => {
+  //       reg.update();
+  //     });
+  //   }
+  // });
   return (
     <div
       style={{
