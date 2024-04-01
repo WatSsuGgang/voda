2ac9a85e8848737558be51bdacbb2500d117632c
이수민 <suminjeff@gmail.com>
2024-04-01 09:21:46 +0900

    fix: Fix calendar style

--- frontend/voda/src/pages/calendar/CalendarPage.jsx
+++ frontend/voda/src/pages/calendar/CalendarPage.jsx
@@ -3,18 +3,18 @@ import Calendar from "../../components/calendar/Calendar";
 
 const CalendarPage = () => {
   return (
-    <div
-      style={{
-        marginTop: "3rem",
-      }}
-    >
-      <h2
+    <div>
+      <h3
         style={{
-          textAlign: "center",
+          backgroundColor: "#FFFAE1",
+          height: "3rem",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
         }}
       >
         감정 캘린더
-      </h2>
+      </h3>
       <Calendar />
     </div>
   );
