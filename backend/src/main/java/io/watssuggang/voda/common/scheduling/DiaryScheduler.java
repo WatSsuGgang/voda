f7a7639f2c2fd3884ca16fd2394d65c2fb43e68c
sikutisa <ilgooinkorea@gmail.com>
2024-04-02 14:50:51 +0900

    fix: update schedule

--- backend/src/main/java/io/watssuggang/voda/common/scheduling/DiaryScheduler.java
+++ backend/src/main/java/io/watssuggang/voda/common/scheduling/DiaryScheduler.java
@@ -18,7 +18,7 @@ public class DiaryScheduler {
      * 사용자가 하루에 일기를 쓴 횟수를 매일 자정에 초기화
      */
 
-    @Scheduled(cron = "0 0 0/1 * * *")
+    @Scheduled(cron = "0 0 0 * * *")
     public void deleteKeysWithTodaySchedule() {
         diaryService.deleteKeysWithToday();
         log.info("delete Keys With Today" + LocalDateTime.now());
